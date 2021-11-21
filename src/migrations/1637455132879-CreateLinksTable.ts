e3e297b9b8c1426680a22d03847bd0c756efb35b
Nick C <me@nickc.io>
2021-11-20 20:35:16 -0500

    chore: Fix lint error

--- src/migrations/1637455132879-CreateLinksTable.ts
+++ src/migrations/1637455132879-CreateLinksTable.ts
@@ -1,14 +1,14 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner} from 'typeorm';
 
 export class CreateLinksTable1637455132879 implements MigrationInterface {
     name = 'CreateLinksTable1637455132879'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "links" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "href" character varying NOT NULL, "icon" character varying NOT NULL, CONSTRAINT "PK_ecf17f4a741d3c5ba0b4c5ab4b6" PRIMARY KEY ("id"))`);
+        await queryRunner.query('CREATE TABLE "links" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "href" character varying NOT NULL, "icon" character varying NOT NULL, CONSTRAINT "PK_ecf17f4a741d3c5ba0b4c5ab4b6" PRIMARY KEY ("id"))');
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE "links"`);
+        await queryRunner.query('DROP TABLE "links"');
     }
 
 }
