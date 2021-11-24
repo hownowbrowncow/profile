f6ff2191732192a63eb01ff2384698eac4cc6e8d
Nick C <me@nickc.io>
2021-11-24 17:16:09 -0500

    feat: Add custom mui theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,12 +14,29 @@ import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import CssBaseline from '@mui/material/CssBaseline';
 import Toolbar from '@mui/material/Toolbar';
+import {createTheme, ThemeProvider} from '@mui/material/styles';
 
 import DrawerNav from 'components/Drawer/DrawerNav';
 import DrawerToolbar from 'components/Drawer/DrawerToolbar';
 import {AppContext, AppContextState, defaultAppContext} from 'contexts/AppContext';
 
 const queryClient = new QueryClient();
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#ffffff',
+      main: '#f5f5f5',
+      dark: '#c2c2c2',
+      contrastText: '#000000',
+    },
+    secondary: {
+      light: '#ffffff',
+      main: '#cfd8dc',
+      dark: '#9ea7aa',
+      contrastText: '#000',
+    },
+  },
+});
 
 function App({Component, pageProps: {session, ...pageProps}}: AppProps) {
   const [isLoading, setIsLoading] = useState(false);
@@ -43,49 +60,51 @@ function App({Component, pageProps: {session, ...pageProps}}: AppProps) {
   }, [isLoading, isAuthed]);
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools />
-      <SessionProvider session={session}>
-        <SnackbarProvider
-          autoHideDuration={3000}
-          anchorOrigin={{
-            vertical: 'top',
-            horizontal: 'center',
-          }}
-        >
-          <AppContext.Provider value={appContext}>
-            <CssBaseline />
-            <DrawerToolbar
-              drawerWidth={drawerWidth}
-              handleDrawerToggle={handleDrawerToggle}
-            />
-            <Container>
-              <Box sx={{display: 'flex'}}>
-                <CssBaseline />
-                <Box
-                  component='nav'
-                  sx={{width: {sm: drawerWidth}, flexShrink: {sm: 0}}}
-                  aria-label='mailbox folders'
-                >
-                  <DrawerNav
-                    drawerWidth={drawerWidth}
-                    mobileOpen={mobileOpen}
-                    handleDrawerToggle={handleDrawerToggle}
-                  />
+    <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools />
+        <SessionProvider session={session}>
+          <SnackbarProvider
+            autoHideDuration={3000}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'center',
+            }}
+          >
+            <AppContext.Provider value={appContext}>
+              <CssBaseline />
+              <DrawerToolbar
+                drawerWidth={drawerWidth}
+                handleDrawerToggle={handleDrawerToggle}
+              />
+              <Container>
+                <Box sx={{display: 'flex'}}>
+                  <CssBaseline />
+                  <Box
+                    component='nav'
+                    sx={{width: {sm: drawerWidth}, flexShrink: {sm: 0}}}
+                    aria-label='mailbox folders'
+                  >
+                    <DrawerNav
+                      drawerWidth={drawerWidth}
+                      mobileOpen={mobileOpen}
+                      handleDrawerToggle={handleDrawerToggle}
+                    />
+                  </Box>
+                  <Box
+                    component='main'
+                    sx={{flexGrow: 1, p: 3, width: {sm: `calc(100% - ${drawerWidth}px)`}}}
+                  >
+                    <Toolbar />
+                    <Component{...pageProps} />
+                  </Box>
                 </Box>
-                <Box
-                  component='main'
-                  sx={{flexGrow: 1, p: 3, width: {sm: `calc(100% - ${drawerWidth}px)`}}}
-                >
-                  <Toolbar />
-                  <Component{...pageProps} />
-                </Box>
-              </Box>
-            </Container>
-          </AppContext.Provider>
-        </SnackbarProvider>
-      </SessionProvider>
-    </QueryClientProvider>
+              </Container>
+            </AppContext.Provider>
+          </SnackbarProvider>
+        </SessionProvider>
+      </QueryClientProvider>
+    </ThemeProvider>
   );
 }
 
