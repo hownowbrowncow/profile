2cfeaf2e99ef80d2f7a2e864230114dca8d8106c
Nick C <me@nickc.io>
2021-11-24 21:18:32 -0500

    chore: Migration linting fix

--- src/migrations/1637519321319-AddBioDescription.ts
+++ src/migrations/1637519321319-AddBioDescription.ts
@@ -1,14 +1,14 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner} from 'typeorm';
 
 export class AddBioDescription1637519321319 implements MigrationInterface {
     name = 'AddBioDescription1637519321319'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "public"."bios" ADD "description" character varying NOT NULL`);
+        await queryRunner.query('ALTER TABLE "public"."bios" ADD "description" character varying NOT NULL');
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "public"."bios" DROP COLUMN "description"`);
+        await queryRunner.query('ALTER TABLE "public"."bios" DROP COLUMN "description"');
     }
 
 }
--- src/migrations/1637799988737-AddEmailToBio.ts
+++ src/migrations/1637799988737-AddEmailToBio.ts
@@ -1,14 +1,14 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner} from 'typeorm';
 
 export class AddEmailToBio1637799988737 implements MigrationInterface {
     name = 'AddEmailToBio1637799988737'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "public"."bios" ADD "email" character varying NOT NULL`);
+        await queryRunner.query('ALTER TABLE "public"."bios" ADD "email" character varying NOT NULL');
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "public"."bios" DROP COLUMN "email"`);
+        await queryRunner.query('ALTER TABLE "public"."bios" DROP COLUMN "email"');
     }
 
 }
