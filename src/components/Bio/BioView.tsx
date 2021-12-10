fa03c5144779f74430156e437650c96510b83f5b
Nick C <me@nickc.io>
2021-12-09 20:46:15 -0500

    feat: Implement BioView skeleton

--- src/components/Bio/BioView.tsx
+++ src/components/Bio/BioView.tsx
@@ -1,5 +1,6 @@
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
+import Skeleton from '@mui/material/Skeleton';
 
 import {Bio} from 'entities/Bio';
 
@@ -8,10 +9,32 @@ interface Props {
   isLoading: boolean
 }
 
+export function BioViewSkeleton() {
+  return (
+    <Grid container spacing={0}>
+      <Grid item md={6}>
+        <Skeleton width='50%' />
+      </Grid>
+      <Grid container item md={6}>
+        <Grid item md={12} display='flex' justifyContent='right'>
+          <Skeleton width='60%' />
+        </Grid>
+        <Grid item md={12} display='flex' flexDirection='row' justifyContent='end'>
+          <Skeleton width='40%' />
+        </Grid>
+      </Grid>
+      <Grid item md={12}>
+        <Skeleton variant='rectangular' height={50} />
+      </Grid>
+    </Grid>
+  );
+}
+
 export function BioView(props: Props) {
   const {bio, isLoading} = props;
 
-  return !isLoading && (
+
+  return isLoading ? <BioViewSkeleton /> : (
     <Grid container spacing={0}>
       <Grid item md={6}>
         <Typography variant='h4'>{bio.displayName}</Typography>
